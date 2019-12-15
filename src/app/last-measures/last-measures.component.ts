952b5c1263154c34b893e05dee3a7575826ba8af
pellenberger <p.ellenberger@outlook.com>
2019-12-15 22:29:11 +0100

    Add dropdown in LastMeasures component

--- src/app/last-measures/last-measures.component.ts
+++ src/app/last-measures/last-measures.component.ts
@@ -16,20 +16,35 @@ export interface Measure {
 })
 export class LastMeasuresComponent implements OnInit {
 
-	//public items: Array<number> = [5, 10, 15, 20, 30, 50, 100];
+	public countOptions: Array<number> = [5, 10, 15, 20, 30, 50, 100];
+
+  private selectedCount: number;
+
+  private afs: AngularFirestore;
 
 	private measuresCollection: AngularFirestoreCollection<Measure>;
   measures: Observable<Measure[]>;
 
-  constructor(private afs: AngularFirestore) {
-  	this.measuresCollection = afs.collection<Measure>('measures', ref =>
-			ref
-			.orderBy('datetime', 'desc')
-			.limit(20));  	
+  constructor(afs: AngularFirestore) {
+    this.afs = afs;
+    this.selectedCount = 10;
+  	this.queryCollection();	
   }
 
-  ngOnInit() {
-  	this.measures = this.measuresCollection.valueChanges();
+  ngOnInit() {    
   }
 
+  queryCollection() {
+      this.measuresCollection = this.afs.collection<Measure>('measures', ref =>
+      ref
+      .orderBy('timestamp', 'desc')
+      .limit(this.selectedCount));
+
+      this.measures = this.measuresCollection.valueChanges();
+  }
+
+  selectCountOption(selectedCount: number): void {
+        this.selectedCount = selectedCount;
+        this.queryCollection();
+    }
 }
--- src/app/pings/pings.component.ts
+++ src/app/pings/pings.component.ts
@@ -24,7 +24,7 @@ export class PingsComponent implements OnInit {
 	constructor(private afs: AngularFirestore) {
 		this.pingsCollection = afs.collection<Ping>('pings', ref =>
 			ref
-			.orderBy('datetime', 'desc')
+			.orderBy('timestamp', 'desc')
 			.limit(5));  	
   }
 
